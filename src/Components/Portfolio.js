8a2149249a95caf33c0d0aad7b49ec0f77d6b16d
verycool <verycool@verycools-MacBook-Pro.local>
2021-07-29 14:27:45 -0400

    ggg

---
+++ src/Components/Portfolio.js
@@ -0,0 +1,49 @@
+import React, { Component } from "react";
+import Zmage from "react-zmage";
+import Fade from "react-reveal";
+
+let id = 0;
+class Portfolio extends Component {
+  render() {
+    if (!this.props.data) return null;
+
+    const projects = this.props.data.projects.map(function (projects) {
+      let projectImage = "images/portfolio/" + projects.image;
+
+      return (
+        <div key={id++} className="columns portfolio-item">
+          <div className="item-wrap">
+            <img src={projectImage} style = {{height:160}} />
+            <div style={{ textAlign: "center" }} >
+              <a href = {projects.url}>
+              {projects.title}
+              </a>
+              
+              </div>
+          </div>
+        </div>
+      );
+    });
+
+    return (
+      <section id="portfolio">
+        <Fade left duration={1000} distance="40px">
+          <div className="row">
+            <div className="twelve columns collapsed">
+              <h1>Check Out Some of My Projects.</h1>
+
+              <div
+                id="portfolio-wrapper"
+                className="bgrid-quarters s-bgrid-thirds cf"
+              >
+                {projects}
+              </div>
+            </div>
+          </div>
+        </Fade>
+      </section>
+    );
+  }
+}
+
+export default Portfolio;
